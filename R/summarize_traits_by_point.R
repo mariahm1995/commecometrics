5ead0fcab08e72832fe61f65c9a0b917b5106fd7
Maria A. Hurtado-Materon <maria.h.m_1995@tamu.edu>
2025-05-07 22:22:19 -0500

    continent column was no saved on the output of summary_traits_by_point

--- R/summarize_traits_by_point.R
+++ R/summarize_traits_by_point.R
@@ -153,6 +153,7 @@ summarize_traits_by_point <- function(points_df,
 
     points_sf <- sf::st_as_sf(points_df, coords = c(lon_col, lat_col), crs = sf::st_crs(continent_shp))
     joined <- sf::st_join(points_sf, continent_shp, left = TRUE)
+    points_df$continent <- joined$continent
   }
   return(list(
     points = points_df,
